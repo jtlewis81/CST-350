9f43eb480d881600d3259601c657436fc7555e2c
jtlewis81 <jtlewis91481@gmail.com>
2023-12-21 20:41:51 -0500

    add topic 5 activity 5

---
+++ Topic 5/Activity 5/Applications/ASPCoreFirstApp/Controllers/ProductsAPIController.cs
@@ -0,0 +1,82 @@
+﻿using ASPCoreFirstApp.Models;
+using ASPCoreFirstApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http.Description;
+
+namespace ASPCoreFirstApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsAPIController : ControllerBase
+    {
+
+        ProductsDAO repository = new ProductsDAO();
+
+        public ProductsAPIController()
+        {
+            repository = new ProductsDAO();
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(List<ProductDTO>))]
+        public IEnumerable<ProductDTO> Index()
+        {
+            List<ProductModel> productList = repository.AllProducts();
+            IEnumerable<ProductDTO> productDTOs = from p in productList
+                                                  select new ProductDTO(p.Id, p.Name, p.Price, p.Description);
+            return productDTOs;
+        }
+
+        [HttpGet("searchresults/{searchTerm}")]
+        [ResponseType(typeof(List<ProductDTO>))]
+        public IEnumerable<ProductDTO> SearchResults(string searchTerm)
+        {
+            List<ProductModel> productList = repository.SearchProducts(searchTerm);
+            IEnumerable<ProductDTO> productDTOs = from p in productList
+                                                  select new ProductDTO(p.Id, p.Name, p.Price, p.Description);
+            return productDTOs;
+        }
+
+        [HttpGet("showoneproduct/{id}")]
+        [ResponseType(typeof(ProductDTO))]
+        public ActionResult <ProductDTO> ShowOneProduct(int id)
+        {
+            ProductModel product = repository.GetProductById(id);
+            ProductDTO productDTO = new ProductDTO(product.Id, product.Name, product.Price, product.Description);
+            return productDTO;
+        }
+
+        [HttpPut("processedit")]
+        [ResponseType(typeof(List<ProductDTO>))]
+        public IEnumerable<ProductDTO> ProcessEdit(ProductModel product)
+        {
+            repository.Update(product);
+            List<ProductModel> productList = repository.AllProducts();
+            IEnumerable<ProductDTO> productDTOs = from p in productList
+                                                  select new ProductDTO(p.Id, p.Name, p.Price, p.Description);
+            return productDTOs;
+        }
+
+        [HttpPut("processeditreturnoneitem")]
+        [ResponseType(typeof(ProductDTO))]
+        public ActionResult <ProductDTO> ProcessEditReturnOneItem(ProductModel product)
+        {
+            repository.Update(product);
+            ProductModel updatedProduct = repository.GetProductById(product.Id);
+            ProductDTO productDTO = new ProductDTO(updatedProduct.Id, updatedProduct.Name, updatedProduct.Price, updatedProduct.Description);
+            return productDTO;
+        }
+
+        [HttpDelete("delete/{id}")]
+        [ResponseType(typeof(List<ProductDTO>))]
+        public IEnumerable<ProductDTO> Delete(int id)
+        {
+            repository.Delete(id);
+            List<ProductModel> productList = repository.AllProducts();
+            IEnumerable<ProductDTO> productDTOs = from p in productList
+                                                  select new ProductDTO(p.Id, p.Name, p.Price, p.Description);
+            return productDTOs;
+        }
+
+    }
+}
---
+++ Topic 5/Activity 5/Applications/ButtonGrid/wwwroot/js/site.js
@@ -0,0 +1,77 @@
+﻿$(function () {
+    console.log("Page is Ready");
+
+    $(document).bind("contextmenu", function (e) {
+        e.preventDefault();
+        console.log("context menu hidden on right click!");
+    });
+
+    //$(document).on("click", ".game-button", function (event) {
+    //    event.preventDefault();
+
+    //    var buttonNumber = $(this).val();
+    //    console.log("button " + buttonNumber + " clicked");
+    //    doButtonUpdate(buttonNumber);
+    //});
+
+    $(document).on("mousedown", ".game-button", function (event) {
+        switch (event.which) {
+            case 1:
+                //alert('left click');
+                event.preventDefault();
+                var buttonNumber = $(this).val();
+                console.log("button " + buttonNumber + " left clicked");
+                doButtonUpdate(buttonNumber, "/button/ShowOneButton");
+                break;
+            case 2:
+                alert('middle click');
+                break;
+            case 3:
+                //alert('right click');
+                event.preventDefault();
+
+                var buttonNumber = $(this).val();
+                console.log("button " + buttonNumber + " right clicked");
+                doButtonUpdate(buttonNumber, "/button/RightClickShowOneButton");
+                break;
+            default:
+                alert('not a click');
+        }
+    });
+
+
+});
+
+function doButtonUpdate(buttonNumber, urlString) {
+    $.ajax({
+        datatype: "json",
+        method: 'POST',
+        url: urlString,
+        data: {
+            "buttonNumber": buttonNumber
+        },
+        success: function (data) {
+            console.log(data);
+            $("#" + buttonNumber).html(data);
+            checkForMatch();      
+        }
+    });
+};
+
+function checkForMatch() {
+    var gameWon = true;
+    var state = $(".game-button:first").data("state");
+    $(".game-button").each(function () {
+        if ($(this).data("state") !== state) {
+            gameWon = false;
+        }
+    });
+
+    if (gameWon === true) {
+        $("#gameMessage").html("Congrats! You Win!");
+    }
+    else {
+        $("#gameMessage").html("Try to match all the buttons!");
+    }
+
+};
