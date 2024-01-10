2924e9349ccf2bd4a5772470849cafde8ae63866
jtlewis81 <jtlewis91481@gmail.com>
2024-01-10 16:40:53 -0500

    add activity 6

---
+++ Topic 6/Activity 6/Applications/RegisterAndLoginApp/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginApp.Models;
+using RegisterAndLoginApp.Services;
+using RegisterAndLoginApp.Utility;
+
+namespace RegisterAndLoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [CustomAuthorization]
+        public IActionResult PrivatePage()
+        {
+            return Content("You should only see this page if you are logged in successfully!");
+        }
+
+        [LogActionFilter]
+        public IActionResult ProcessLogin(UserModel user)
+        {
+            SecurityService securityService = new SecurityService();
+
+            if (securityService.IsValid(user))
+            {
+                MyLogger.GetInstance().Info("Login Success");
+
+                HttpContext.Session.SetString("username", user.UserName);
+
+                return View("LoginSuccess", user);
+            }
+            else
+            {
+                MyLogger.GetInstance().Info("Login Failure");
+
+                HttpContext.Session.Remove("username");
+
+                return View("LoginFailure", user);
+            }
+        }
+    }
+}
---
+++ Topic 6/Activity 6/Applications/RegisterAndLoginApp/Utility/MyLogger.cs
@@ -0,0 +1,48 @@
+﻿using NLog;
+
+namespace RegisterAndLoginApp.Utility
+{
+    public class MyLogger : ILogger
+    {
+        private static MyLogger instance;
+        private static Logger logger;
+
+        public static MyLogger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new MyLogger();
+            }
+            return instance;
+        }
+
+        private Logger GetLogger()
+        {
+            if (logger == null)
+            {
+                logger = LogManager.GetLogger("Activity6LoggerRule");
+            }
+            return logger;
+        }
+
+        public void Debug(string message)
+        {
+            GetLogger().Debug(message);
+        }
+
+        public void Error(string message)
+        {
+            GetLogger().Error(message);
+        }
+
+        public void Info(string message)
+        {
+            GetLogger().Info(message);
+        }
+
+        public void Warning(string message)
+        {
+            GetLogger().Warn(message);
+        }
+    }
+}
